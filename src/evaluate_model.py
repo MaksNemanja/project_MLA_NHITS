2b5ef1e809385ff7817f03a076715c9b73870b81
YounesHouhou <133326039+YounesHouhou@users.noreply.github.com>
2025-01-04 00:54:18 +0100

    Update evaluate_model.py

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -20,23 +20,6 @@ def parse_args():
 
     return args
 
-def define_space(args):
-
-    space = {
-    'batch_size': hp.choice('batch_size', [256]),
-    'kernel_size': hp.choice('kernel_size',[[2,2,2],[4,4,4],[8,8,8],[8,4,1],[16,8,1]]),
-    'expressiveness_ratio': hp.choice('expressiveness_ratio', [[1./168, 1./24, 1.], [1./24, 1./12, 1.], [1./180, 1./60, 1.],[1./40, 1./20, 1.],[1./64, 1./8, 1.]]),
-    'nb_stack': hp.choice('nb_stack', [1]),  
-    'nb_block': hp.choice('nb_block', [3]),  
-    'input_size': hp.choice('input_size', [args.horizon*5]),     
-    'horizon': hp.choice('horizon', [args.horizon]),           
-    'hidden_sizes': hp.choice('hidden_sizes', [[512,512]]),  
-    'learning_rate': hp.choice('learning_rate',[1e-3]),  
-    'random_seed': hp.quniform('random_seed', 1, 10, 1)
-    }
-
-    return space 
-
 def main(args):
 
     # ----------------- Import hyperopt Trial object from file ---------------------
